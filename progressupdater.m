404fffd19b854ac9b814f4031ed80cb698372e3b
rajanprasad460 <80826197+rajanprasad460@users.noreply.github.com>
2022-08-04 14:00:00 +0400

    Module Explanation added into code

--- progressupdater.m
+++ progressupdater.m
@@ -1,4 +1,8 @@
 function str=progressupdater(i,row,str_maj)
+% function str=progressupdater(i,row,str_maj)
+% progressupdater reads the current loop count, total loop, and user
+% defined string to be displayed as an update to the user
+
 s_progress=blanks(50);
 progress=round(i/row*50);
 progress2 = (i/row*50);
