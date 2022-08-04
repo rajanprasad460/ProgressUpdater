f413159a5ceb454544d058371db1ec9502ee4dfa
rajanprasad460 <80826197+rajanprasad460@users.noreply.github.com>
2022-08-04 13:58:17 +0400

    intial version of the progressupdator

---
+++ progressupdater.m
@@ -0,0 +1,14 @@
+function str=progressupdater(i,row,str_maj)
+s_progress=blanks(50);
+progress=round(i/row*50);
+progress2 = (i/row*50);
+s_progress(1:progress)=repelem('*',progress);
+a_progress=sprintf('%.2f%%',progress2*2);
+s_progress(21:21+length(a_progress)-1)=a_progress;
+
+clc;
+str{1}=str_maj;
+str{2}=sprintf('[%s] Complete \n',s_progress);
+% fprintf('[%s] Complete \n',s_progress);
+fprintf('%s\n%s',str{1},str{2});
+end
